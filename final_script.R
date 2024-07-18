1e6f4bde0d95c6f1a8647ef4fb166ab9303199d5
Maria Luiza Campos <157021554+medydcampos@users.noreply.github.com>
2024-07-18 13:50:14 +0100

    Add files via upload

---
+++ final_script.R
@@ -0,0 +1,16 @@
+#### FINAL SCRIPT ###
+
+# Installing packages  ----------------------------------------------------
+
+install.packages("betareg")
+install.packages("psych")
+install.packages("stargazer")
+
+## Loading packages 
+
+library(car)
+library(betareg)
+library(psych)
+library(tidyverse)
+library(readxl)
+library(stargazer)
